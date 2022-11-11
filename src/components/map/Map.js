2fa6e04235c1e50fc0c487cfd55b4c16a19f6d1c
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-11-11 12:05:09 +0100

    Add hooks and fetch function

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -8,7 +8,7 @@ const Map = () => {
     const position = [51.505, -0.09]
   return (
     
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height: "100%", width: "100vw"}}>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{height: "100%", width: "100vw"}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,26 +1,45 @@
-import React from 'react'
+import React from "react";
 import { GlobalStyle } from "assets/styles/globalStyles";
 import { theme } from "assets/styles/themes";
 import { ThemeProvider } from "styled-components";
 import { Wrapper } from "./Root.styles";
-import Search from 'components/search/Search';
-import Map from 'components/map/Map';
+import { useState, useEffect } from "react";
+import Search from "components/search/Search";
+import Map from "components/map/Map";
 
 const Root = () => {
+  const [address, setAddress] = useState(null);
+  const [ipAddress, setIpAddress] = useState("");
+  
+
+  const getData = async () => {
+    try {
+      const res = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=192.212.174.101`
+      );
+      const data = await res.json();
+      setAddress(data);
+      console.log(data);
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Wrapper>
-        <Search />
-        <Map />
-      </Wrapper>
-    </ThemeProvider>
-  </>
-  )
-}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+          <Search />
+          <Map />
+        </Wrapper>
+      </ThemeProvider>
+    </>
+  );
+};
 
-export default Root
-
-
-// https://geo.ipify.org/api/v2/country,city?apiKey=at_276wt6TR4TRkQPfLUzHyHrasToTmt&ipAddress=8.8.8.8
+export default Root;
